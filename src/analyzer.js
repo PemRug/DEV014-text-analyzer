72160a77a48f9341d192879917976ca5843b272e
PemRug <paolamejia955@gmail.com>
2024-02-27 23:56:00 -0500

    Commit para 1er Spring

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,127 @@
 const analyzer = {  
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    let texto = text.value.trim();
+  let contadorPalabras = 0;
+  const caracteresInvalidos = ["..",".","#",",",";",":","?","!","¡","¿","'","...","-","_"];
+  let palabrasTipoString = [];
+  let numerosTipoString = [];
+
+  const palabras = texto.split(" ");
+
+  for (let i = 0; i < palabras.length; i++) {
+    if (!caracteresInvalidos.includes(palabras[i])) {
+      if (!isNaN(palabras[i])) {
+        numerosTipoString.push(palabras[i]);
+      } else {
+        palabrasTipoString.push(palabras[i]);
+      }
+    }
+  }
+  contadorPalabras = palabrasTipoString.length;
+
+    const contador1 = document.querySelector('ul li.contadorP');
+    contador1.textContent = `Palabras: ${contadorPalabras}`;
+
+    console.log(contador1.textContent);
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    let texto = text.value;
+    let contadorCaracteres = texto.length;
+  
+    const contador2 = document.querySelector('ul li.contadorC');
+    if (contador2 !== null) {
+      contador2.textContent = `Caracteres: ${contadorCaracteres}`;
+    } else {
+      console.error("Error: Elemento con clase 'contadorC' no encontrado");
+    }
+    console.log(contador2.textContent);
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    let texto = text.value;
+    let contadorCaracteresSinEspacio = 0;
+    
+    for (let i=0; i < texto.length; i++) {
+      if (texto.charAt(i) !== " ") {
+        contadorCaracteresSinEspacio++;
+      }
+    }
+    
+    const contador3 = document.querySelector('ul li.contadorCSE');
+      if (contador3 !== null) { 
+      contador3.textContent = `Caracteres sin espacios: ${contadorCaracteresSinEspacio}`;
+    } else {
+      console.error("Error: Elemento con clase 'contadorCSE' no encontrado");
+    }
+    console.log(contador3.textContent);
+    return contadorCaracteresSinEspacio;
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    let texto = text.value.trim();
+    let palabras = texto.split(" ");
+    let largoTotal = 0;
+
+    for (let i = 0; i < palabras.length; i++) {
+      largoTotal += palabras[i].length;
+    }
+
+    const promedioTotal = (largoTotal / palabras.length).toFixed(2);
+
+    let contador6 = document.querySelector('ul li.contadorPR');
+  if (contador6 !== null) {
+    contador6.textContent = `Promedio de longitud:: ${promedioTotal}`;
+  } else {
+    console.error("Error: Elemento 'Promedio de longitud:' no encontrado");
+  }
+  console.log(contador6.textContent); 
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    let texto = text.value;
+    const numeros = [];
+  
+    for (let i = 0; i < texto.length; i++) {
+      if (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."].includes(texto[i])) {
+        let numero = "";
+        do {
+          numero += texto[i];
+          i++;
+        } while (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."].includes(texto[i]) && i < texto.length);
+        if (numero !== "") {
+          numeros.push(Number(numero)); 
+        }
+        i--;
+      }
+    }
+    const contadorNumeros = numeros.length;
+  
+    let contador4 = document.querySelector('ul li.contadorN');
+    if (contador4 !== null) {
+      contador4.textContent = `Números: ${contadorNumeros}`;
+    } else {
+      console.error("Error: Elemento 'Numeros' no encontrado");
+    }
+    return numeros; 
   },
-  getNumberSum: (text) => {
+  getNumberSum: (numeros) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    let suma = 0;
+
+    for (let i = 0; i < numeros.length; i++) {
+      suma += numeros[i];
+    }
+  
+    let contador5 = document.querySelector('ul li.contadorS');
+    if (contador5 !== null) {
+      contador5.textContent = `Sumatoria: ${suma}`;
+    } else {
+      console.error("Error: Elemento 'sumatoriaNumeros' no encontrado");
+    }
+  
+    console.log(contador5.textContent); 
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -2,122 +2,23 @@ import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
-//Convertir string en enteros del text area
-
-let inputText = document.querySelector('textarea[name="user-input"]');
-inputText.addEventListener('input', () => {
-    getWordCount(inputText);
-    getCharacterCount(inputText);
-    getCharacterCountExcludingSpaces(inputText);
-    getNumbersCount(inputText);
-    getNumberSum(inputText);
-    console.log(inputText);
+let text = document.querySelector('textarea[name="user-input"]');
+let numeros;
+text.addEventListener('input', () => {
+  numeros = analyzer.getNumberCount(text);
+    analyzer.getWordCount(text);
+    analyzer.getCharacterCount(text);
+    analyzer.getCharacterCountExcludingSpaces(text);
+    analyzer.getNumberSum(numeros);
+    analyzer.getAverageWordLength(text); 
 });
 
 const boton = document.getElementById("reset-button");
+const lisOriginal = '<li class="contadorP">Palabras: </li><li class="contadorC">Caractéres:</li><li class="contadorCSE">Caractéres sin espacios:</li><li class="contadorN">Números:</li><li class="contadorS">Sumatoria:</li><li class="contadorPR">Promedio de longitud:</li>'
 boton.addEventListener('click', () => {
-    inputText.value = " ";
+    text.value = " ";
+    const lis = document.querySelector('ul');
+    if (lis) {
+      lis.innerHTML = lisOriginal;
+    }
 });
-
-//Función Contador de Palabras
-
-function getWordCount(inputText) {
-    let texto = inputText.value;
-    texto= texto.trim();
-    let separadorPalabras = texto.split(" ");
-    let contadorPalabras = 0;
-  
-    separadorPalabras.forEach(() => {
-      if (!isNaN(inputText)) {
-       if (typeof inputText !== "symbol") {
-        contadorPalabras++;
-       }
-      }
-    });
-  
-    const contador1 = document.querySelector('ul li.contadorP');
-    contador1.textContent = `Palabras: ${contadorPalabras}`;
-
-    console.log(contador1.textContent);
-}
-
-//Función Contador de Caractéres
-
-function getCharacterCount(inputText) {
-    let texto = inputText.value;
-    texto= texto.trim();
-    let contadorCaracteres = texto.length;
-  
-    const contador2 = document.querySelector('ul li.contadorC');
-    if (contador2 !== null) {
-      contador2.textContent = `Caracteres: ${contadorCaracteres}`;
-    } else {
-      console.error("Error: Elemento con clase 'contadorC' no encontrado");
-    }
-    console.log(contador2.textContent);
-}
-
-//Función Contador de Caractéres Sin Espacios
-
-function getCharacterCountExcludingSpaces(inputText) {
-let texto = inputText.value;
-texto = texto.replace(/\s/g, "");
-let contadorCaracteresSinEspacio = 0;
-
-for (let i=0; i < texto.length; i++) {
-  if (texto.charAt(i) !== " ") {
-    contadorCaracteresSinEspacio++;
-  }
-}
-
-const contador3 = document.querySelector('ul li.contadorCSE');
-  if (contador3 !== null) { 
-  contador3.textContent = `Caracteres sin espacios: ${contadorCaracteresSinEspacio}`;
-} else {
-  console.error("Error: Elemento con clase 'contadorCSE' no encontrado");
-}
-console.log(contador3.textContent);
-}
-
-//Función Contador de Números
-
-function getNumbersCount(inputText) {
-  let texto = inputText.value;
-  let contadorNumeros = 0;
-  
-for (let i=0; i <texto.length; i++) {
-  if(!isNaN(Number(texto.charAt(i))) && texto.charAt(i) !== " ") {
-    contadorNumeros++;
-  }
-}
-let contador4 = document.querySelector('ul li.contadorN');
-if (contador4 !== null) {
-  contador4.textContent = `Números: ${contadorNumeros}`;
-}  else {
-    console.error("Error: Elemento 'Numeros' no econtrado");
-  }
-  console.log(contador4.textContent);
-}
-
-//Función Sumatoria de Número
-function getNumberSum(inputText) {
-  let texto = inputText.value;
-  let sumatoriaNumeros = getNumbersCount(inputText); 
-  let suma = 0;
-
- 
-  for (let i = 0; i < texto.length; i++) {
-    if (!isNaN(Number(texto.charAt(i))) && texto.charAt(i) !== " ") {
-      suma += Number(texto.charAt(i));
-    }
-  }
-
-  let contador5 = document.querySelector('ul li.contadorS');
-  if (contador5 !== null) {
-    contador5.textContent = `Sumatoria: ${suma}`;
-  } else {
-    console.error("Error: Elemento 'sumatoriaNumeros' no encontrado");
-  }
-
-  console.log(contador5.textContent);
-}
